8352a9f60e3c155c35e376c69471d14d205041a4
Dmytrii Datskevych <datskevychdv@gmail.com>
2025-08-08 19:00:22 +0200

    Add files for our furnitures section

---
+++ src/js/refs.js
@@ -0,0 +1,15 @@
+export default {
+    categories: document.querySelector('.our-furnitures__categories'),
+    categoriesItems: document.querySelectorAll('.categories__item'),
+    categoriesTumbs: document.querySelectorAll('.categories__thumb'),
+    furnitures: document.querySelector('.our-furnitures__list'),
+    loadMore: document.querySelector('.our-furnitures__load-more-btn'),
+    //---------------modal-----------------------------------
+    modal: document.querySelector('.modal'),
+    modalFurniture: document.querySelector('.modal-furniture'),
+    openOredrBtn: document.querySelector('.modal-furniture__btn'),
+    modalCloseBtn: document.querySelector('.modal__close-btn'),
+
+    spinner: document.querySelector('.loader-box')
+
+};
