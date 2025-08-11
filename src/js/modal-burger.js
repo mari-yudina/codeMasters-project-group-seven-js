bd822351dd95f73b417846b38131c89931b1e08b
Oksana Bochkor <oksanabockor785@gmail.com>
2025-08-11 08:56:04 +0300

    header

--- src/js/modal-burger.js
+++ src/js/modal-burger.js
@@ -6,18 +6,27 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     if (target) {
       target.scrollIntoView({ behavior: "smooth" });
     }
-    closeMobileMenu();
+
+    if (mobileMenu.classList.contains("is-open")) {
+      closeMobileMenu();
+    }
   });
 });
-
 
 const burgerBtn = document.querySelector(".burger-btn");
 const mobileMenu = document.querySelector(".mobile-menu");
 const closeBtn = document.querySelector(".mobile-close-btn");
 
+function handleEscape(e) {
+  if (e.key === "Escape") {
+    closeMobileMenu();
+  }
+}
+
 burgerBtn.addEventListener("click", () => {
   mobileMenu.classList.add("is-open");
   document.body.classList.add("no-scroll");
+  document.addEventListener("keydown", handleEscape); // додаємо тільки при відкритті
 });
 
 closeBtn.addEventListener("click", closeMobileMenu);
@@ -26,13 +35,8 @@ mobileMenu.addEventListener("click", e => {
   if (e.target === mobileMenu) closeMobileMenu();
 });
 
-
-document.addEventListener("keydown", e => {
-  if (e.key === "Escape") closeMobileMenu();
-});
-
-
 function closeMobileMenu() {
   mobileMenu.classList.remove("is-open");
   document.body.classList.remove("no-scroll");
+  document.removeEventListener("keydown", handleEscape); // знімаємо при закритті
 }
